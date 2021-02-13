8dc5ed27d4c8548fd6606fe606aca22a53d7c802
Pratik Majumdar <majumderpratik15@gmail.com>
2021-02-13 18:05:27 +0530

    seed database and basic index route

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const   express         = require('express'),
         app             = express(),
         methodOverride  = require('method-override'),
         path            = require('path'),
-        mongoose        = require('mongoose')
+        mongoose        = require('mongoose'),
+        Campground      = require('./models/campground')
 
+mongoose.connect("mongodb://localhost:27017/yelp-camp",{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+})
+
+const db =  mongoose.connection;
+db.on('error', console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("Database connected")
+})
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:true}))
+
+app.get('/campgrounds', async (req,res)=>{
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index',{campgrounds})
+})
 
 const PORT = 3000;
 app.listen(PORT,()=>{
